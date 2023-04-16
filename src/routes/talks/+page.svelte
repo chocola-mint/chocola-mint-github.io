d0f71822c07d898632ad09b8bc78c8cedae60c23
CHM <peterconnected@gmail.com>
2023-04-16 12:47:02 +0800

    Added new talk

--- src/routes/talks/+page.svelte
+++ src/routes/talks/+page.svelte
@@ -1,8 +1,41 @@
+<style>
+    .content{
+        display:flex;
+        flex-direction: column;
+        align-items:center;
+        justify-content: center;
+        width: 100%;
+
+    }
+    iframe{
+        width: 100%;
+    }
+</style>
 <script>
+	import Button, { Label } from "@smui/button";
+
 
 	import Paper, { Content, Title } from "@smui/paper";
 
 </script>
 <Paper>
-    <Title>Coming soon...</Title>
+    <Title>A PROGRAMMER tried to use Visual Scripting!</Title>
+    <Content>
+        📅 Date: 4/16/2023
+        <br>
+        🌏 Location: unity1week online 共有会 #11 (YouTube Live / Zoom)
+        <br>
+        💬 Language: JP
+        <br><br>
+        In this presentation, I talked about the practical issues that I encountered when trying out 
+        Unity's Visual Scripting in its intended usage - using C# to support and extend Visual Scripting functionalities.
+        <br><br>
+        <Button variant="raised" href="https://www.youtube.com/live/XNMWWQH5f9k?feature=share&t=2590" target="_blank" rel="noopener noreferrer">
+            <Label>Video Archive</Label>
+        </Button>
+    </Content>
+    <br>
+    <div class="content">
+        <iframe title="Visual Scripting" src="https://docs.google.com/presentation/d/e/2PACX-1vT1sFPQsyQP4rCOq99_thZdT9_Vxqdocq-nre69-EI5oI6Jr5v8DlClNrSj6oKY0WNJ77lSCdKg0Kxs/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allow="fullscreen"></iframe>
+    </div>
 </Paper>
